4af1cb4c11bc1bbaa0905d9adf448da9e6d88c05
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-08 20:17:46 +0700

    34. Writing More Complex JSX Code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-ExpenseItem.propTypes = {
-    
-};
+ExpenseItem.propTypes = {};
 
 function ExpenseItem(props) {
-    return (
-        <h2>
-            Expense Item
-        </h2>
-    );
+  return (
+    <div>
+      <div>August 06th 2022</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
