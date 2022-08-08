f6f2b3f63153ea1c4962abf509bb8bb561d472d9
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-08 20:12:47 +0700

    33. Building a First Custom Component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+ExpenseItem.propTypes = {
+    
+};
+
+function ExpenseItem(props) {
+    return (
+        <h2>
+            Expense Item
+        </h2>
+    );
+}
+
+export default ExpenseItem;
