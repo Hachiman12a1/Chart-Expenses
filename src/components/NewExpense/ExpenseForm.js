5604b5d2d56a446aaac271736781bf4bb8360d16
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-09 16:28:52 +0700

    52. Adding Form Inputs

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./ExpenseForm.css";
+
+ExpenseForm.propTypes = {};
+
+function ExpenseForm(props) {
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label htmlFor="">Title</label>
+          <input type="text" />
+        </div>
+
+        <div className="new-expense__control">
+          <label htmlFor="">Amount</label>
+          <input type="number" min="0.01" step="0.01" />
+        </div>
+
+        <div className="new-expense__control">
+          <label htmlFor="">Date</label>
+          <input type="date" min="2019-01-01" step="2023-12-31" />
+        </div>
+      </div>
+
+      <div className="new-expense__actions">
+        <button type="submit">Add expense</button>
+      </div>
+    </form>
+  );
+}
+
+export default ExpenseForm;
