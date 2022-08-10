5adbf7958814aa1322911665f310364f277a017d
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-10 15:20:23 +0700

    69. Demo App: Adding a Chart

---
+++ src/components/Chart/ChartBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+ChartBar.propTypes = {
+    
+};
+
+function ChartBar(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default ChartBar;
